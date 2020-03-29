b956177b4cf2022cd9108f6e8de4482103ff5554
prashant ranjan <prashantranjan@prashants-MacBook-Air.local>
2020-03-29 12:26:58 +0530

    Updates

---
+++ precache-manifest.a769d7603ef7b33f0e3d95195efa3df3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c337315db20b4f723964568ec78a0f93",
+    "url": "/trello-board/index.html"
+  },
+  {
+    "revision": "eb8c8a10240ca0a60c53",
+    "url": "/trello-board/static/css/main.28d741df.chunk.css"
+  },
+  {
+    "revision": "7ccacfde67c75c22fd2b",
+    "url": "/trello-board/static/js/2.7ebc85b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trello-board/static/js/2.7ebc85b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8c8a10240ca0a60c53",
+    "url": "/trello-board/static/js/main.adaf9cb2.chunk.js"
+  },
+  {
+    "revision": "79291af1daa39c576bbe",
+    "url": "/trello-board/static/js/runtime-main.3c2e791f.js"
+  },
+  {
+    "revision": "bf2628e43f68f292fa97d1a51a9b0a73",
+    "url": "/trello-board/static/media/cross-icon.bf2628e4.png"
+  },
+  {
+    "revision": "aba9de2d23eff684aab3d2d4bbde5f30",
+    "url": "/trello-board/static/media/plus.aba9de2d.png"
+  }
+]);
